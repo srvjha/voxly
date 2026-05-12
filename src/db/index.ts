0cbb27385fe4eeaf2717f024d529e2e799e778b5
Saurav Jha <srvtj629@gmail.com>
2026-05-12 12:49:56 +0530

    db changed to postgres meri marzi

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,15 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import { drizzle } from "drizzle-orm/node-postgres";
+import dotenv from "dotenv";
 dotenv.config();
+export const db = drizzle(process.env.DATABASE_URL!);
 
-export async function connectDB(){
-    const databaseUrl= process.env.DATABASE_URL!;
-    try {
-        await mongoose.connect(databaseUrl)
-        console.log("Mongo Connected✅")
-    } catch (error:any) {
-        console.log(`Error occured while connecting db: ${error.message}`)
-        process.exit(1);
-    }
+export async function connectDB() {
+   try {
+     const result = await db.execute("SELECT 1")
+    if (result) console.log("Db connected ✅")
+   }
+   catch (error:any) {
+     console.log(`Error occured while connecting db: ${error.message}`)
+     process.exit(1);
+   }
 }
